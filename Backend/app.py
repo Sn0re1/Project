acb47870116a83a6b5650a9c9a91c0a0b6b592d6
Sn0re1 <snore442@gmail.com>
2025-07-10 20:37:29 +0100

    Update app.py
    updating my python file and I have used flask framework in python.

--- Backend/app.py
+++ Backend/app.py
@@ -1,1 +1,10 @@
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
+import json
+import os
 
+
+app = Flask(__name__, static_folder="../frontend", static_url_path="")
+CORS(app)
+
+DATA_FILE = "products.json"
