ce752773bfe8e9c4a08d17d403f3f454806b8805
Sn0re1 <snore442@gmail.com>
2025-07-11 20:23:28 +0100

    Update app.py
    added loading and savings function in

--- Backend/app.py
+++ Backend/app.py
@@ -8,3 +8,14 @@ app = Flask(__name__, static_folder="../frontend", static_url_path="")
 CORS(app)
 
 DATA_FILE = "products.json"
+def load_products():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_products(products):
+    with open(DATA_FILE, "w") as file:
+        json.dump(products, file, indent=4)
+
